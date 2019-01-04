764f366d85b39e30a2691e29ec7bf3138d53da31
Vincent Daranyi <vincent@daranyi.com>
2019-01-04 20:46:45 +0000

    Navbar logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,12 @@ module.exports = {
     description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
